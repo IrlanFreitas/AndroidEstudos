8eb5316c0bc4c462fb4de676a985d0b537c3f988
Irlan Freitas <irlan.freitas@hotmail.com>
2018-07-10 14:45:19 -0300

    Utilizando Menus
    Utilizando o RelativeLayout e a sua forma de organizar os elementos.
    Intent e o que pode se fazer com ela inicialmente.
    Inflate e o conceito de transformar xml em view.

--- app/src/main/java/br/com/alura/agenda/activities/FormularioActivity.java
+++ app/src/main/java/br/com/alura/agenda/activities/FormularioActivity.java
@@ -2,6 +2,9 @@ package br.com.alura.agenda.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -20,22 +23,50 @@ public class FormularioActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_formulario);
 
-        aluno = obterDados();
-
-        Button btnSalvar = findViewById(R.id.formulario_btn_salvar);
-
         /*Setando o comportamento de click do botão com o listener(escutador)
         * de evento de click, poderia ser outros, double click, long click*/
-        btnSalvar.setOnClickListener(new View.OnClickListener() {
+        /* findViewById(R.id.formulario_btn_salvar).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
+            }
+        });*/
+
+    }
+
+    //Modificando a ActionBar assim que é criada
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        //Inflar é transformar o xml em view
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu_formulario, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    //Sobrecrevendo o método que é chamado quando um item de menu da Action Bar é clicado
+    //Forma mais elegante de fazer, poderia ter pego o id do item de menu
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.formulario_menu_item_adicionar: {
+
                 aluno = obterDados();
+
                 //Mensagem que aparece abaixo como uma notificação
                 //FormularioActivity.this = view.getContext()
                 Toast.makeText(FormularioActivity.this, aluno.toString(), Toast.LENGTH_SHORT).show();
-            }
-        });
 
+                //Finalizando a activity com o conceito de pilha do Android
+                finish();
+
+                break;
+            }
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
     private Aluno obterDados() {
--- app/src/main/java/br/com/alura/agenda/activities/ListaAlunosActivity.java
+++ app/src/main/java/br/com/alura/agenda/activities/ListaAlunosActivity.java
@@ -1,8 +1,11 @@
 package br.com.alura.agenda.activities;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 
 import br.com.alura.agenda.R;
@@ -34,6 +37,17 @@ public class ListaAlunosActivity extends AppCompatActivity {
 
         //Adicionando o Adapter a ListView
         listaAlunos.setAdapter(adapter);
+
+        Button btnAdicionar = findViewById(R.id.lista_alunos_btn_adicionar);
+
+        btnAdicionar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Declarando a intenção para o Android gerenciar.
+                Intent intentIrParaFormulario = new Intent( ListaAlunosActivity.this, FormularioActivity.class );
+                startActivity(intentIrParaFormulario);
+            }
+        });
 
     }
 }
