acfab32f8dbe34b3921f10d7ca905eeba6c68908
Irlan Freitas <irlan.freitas@hotmail.com>
2018-07-13 11:29:43 -0300

    Início do curso Android II
    Usando outras intent implicitas com o ACTION_VIEW e o protocolo Uri,
    geo, sms, tel etc.
    Solicitando permissões com o AndroidManifest.xml.
    Fazendo a soliticação de forma programatica por conta do Android 6, modificações na forma de solicitar permissão ao usuário.

--- app/src/main/java/br/com/alura/agenda/activities/ListaAlunosActivity.java
+++ app/src/main/java/br/com/alura/agenda/activities/ListaAlunosActivity.java
@@ -1,6 +1,12 @@
 package br.com.alura.agenda.activities;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.PermissionInfo;
+import android.net.Uri;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.ContextMenu;
@@ -13,6 +19,7 @@ import android.widget.ListView;
 import android.widget.Toast;
 
 import java.io.Serializable;
+import java.security.Permission;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -26,6 +33,8 @@ public class ListaAlunosActivity extends AppCompatActivity {
     private AlunoDAO alunoDAO;
 
     private ListView listaAlunos;
+
+    private final Integer CODIGO_REQUISICAO_LIGACAO = 9090;
 
     //Método sobrescrito baseado no ciclo de vida da Atividade (Activity)
     @Override
@@ -61,7 +70,8 @@ public class ListaAlunosActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 //Declarando a intenção para o Android gerenciar.
-                Intent intentIrParaFormulario = new Intent( ListaAlunosActivity.this, FormularioActivity.class );
+                //Intent Explícita é declarado exatamente de onde você está e quer ir;
+                Intent intentIrParaFormulario = new Intent(ListaAlunosActivity.this, FormularioActivity.class);
                 startActivity(intentIrParaFormulario);
             }
         });
@@ -98,7 +108,74 @@ public class ListaAlunosActivity extends AppCompatActivity {
     @Override
     public void onCreateContextMenu(ContextMenu menu, View v, final ContextMenu.ContextMenuInfo menuInfo) {
 
+        AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) menuInfo;
+        final Aluno aluno = (Aluno) listaAlunos.getItemAtPosition(info.position);
+
         menu.setHeaderTitle("Opções");
+
+
+        MenuItem ligar = menu.add("Ligar");
+        ligar.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem menuItem) {
+
+
+                //Aqui é verificado se a permissão ainda não foi dada, caso não, é solicitada ao usuário
+                if (ActivityCompat.checkSelfPermission(ListaAlunosActivity.this, Manifest.permission.CALL_PHONE)
+                        != PackageManager.PERMISSION_GRANTED) {
+
+                    //RequestPermission é o pop-up que aparece para solicitar permissão
+                    ActivityCompat.requestPermissions(ListaAlunosActivity.this,
+                            new String[]{Manifest.permission.CALL_PHONE},
+                            CODIGO_REQUISICAO_LIGACAO);
+                } else {
+
+                    //Caso essa permissão já tenha sido obtida, é somente feita a chamada.
+
+                    Intent intentLigar = new Intent(Intent.ACTION_CALL);
+                    intentLigar.setData(Uri.parse("tel:" + aluno.getTelefone()));
+                    startActivity(intentLigar);
+                }
+
+
+                return false;
+            }
+        });
+
+        MenuItem enviarSMS = menu.add("Enviar SMS");
+        Intent intentEnviarSMS = new Intent(Intent.ACTION_VIEW);
+        intentEnviarSMS.setData( Uri.parse("sms:"+aluno.getTelefone() ));
+        enviarSMS.setIntent(intentEnviarSMS);
+
+        MenuItem visualizarNoMapa = menu.add("Visualizar no mapa");
+        Intent intentVisualizarMapa = new Intent(Intent.ACTION_VIEW);
+        intentVisualizarMapa.setData( Uri.parse("geo:0,0?q="+aluno.getEndereco() ));
+        visualizarNoMapa.setIntent(intentVisualizarMapa);
+
+        if (aluno.getSite() != null) {
+
+            final MenuItem visitarSite = menu.add("Visitar o site");
+
+            visitarSite.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+                @Override
+                public boolean onMenuItemClick(MenuItem menuItem) {
+
+                    //Intent implícita, você quer essa ação não importando aceite e faça
+                    Intent acessarSite = new Intent(Intent.ACTION_VIEW);
+                    if (!aluno.getSite().startsWith("http://")) {
+                        acessarSite.setData(Uri.parse("http://" + aluno.getSite()));
+                    } else {
+                        acessarSite.setData(Uri.parse(aluno.getSite()));
+                    }
+
+                    //Atalho para fazer o startActivity de outra forma
+                    visitarSite.setIntent(acessarSite);
+
+
+                    return false;
+                }
+            });
+        }
 
         //Poderiamos fazer um inflate com um xml definindo os itens do menu
         //mas será feito de outra forma.
@@ -107,14 +184,29 @@ public class ListaAlunosActivity extends AppCompatActivity {
         deletar.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
             @Override
             public boolean onMenuItemClick(MenuItem menuItem) {
-                AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) menuInfo;
-                Aluno aluno = (Aluno) listaAlunos.getItemAtPosition(info.position);
+
                 alunoDAO.deletar(aluno.getId());
                 carregarLista();
-                Toast.makeText(ListaAlunosActivity.this, "Aluno "+ aluno.getNome()+ " foi deletado com sucesso!", Toast.LENGTH_SHORT).show();
+                Toast.makeText(ListaAlunosActivity.this, "Aluno " + aluno.getNome() + " foi deletado com sucesso!", Toast.LENGTH_SHORT).show();
                 return false;
             }
         });
+
+
+    }
+
+    //Quando solicita permissão de algo que quer fazer algo logo depois,
+    //indicando o código de requisição é possível logo após receber a permissão
+    //realizar algum comportamento
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        //Sabendo qual a permissão está foi solicitada e atendida ou não.
+        //A depois disso acionando uma ação.
+        if (requestCode == CODIGO_REQUISICAO_LIGACAO) {
+
+        }
 
     }
 
